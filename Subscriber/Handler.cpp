e3976a651f54b4a6f01ef553182a61d8954d23c5
antoine <antoine@relativetechnologies.com>
2017-10-31 16:49:04 +1100

    refactor for testing

--- Subscriber/Handler.cpp
+++ Subscriber/Handler.cpp
@@ -1,4 +1,12 @@
 #include "Handler.h"
 #include <iostream>
 
-void Handler::OnQuote(OrderBook &book) { std::cout << book << '\n'; }
+void clear() {
+  // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
+  std::cout << "\x1B[2J\x1B[H";
+}
+
+void Handler::OnQuote(OrderBook &book) {
+  clear();
+  std::cout << book << '\n';
+}
